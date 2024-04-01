9bc78eb1e7b0fdb1f96a664d3c8b88fe99358847
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-04-02 00:04:18 +0300

    Create form of search and add it in views, create template and make urls

--- Notice_board/notice/urls.py
+++ Notice_board/notice/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import notice_detail, notice_list
+from .views import notice_detail, notice_list, search
 
 
 urlpatterns = [
     path('', notice_list, name='not_list'),
     path('<int:not_id>/', notice_detail, name='not_detail'),
+    path('search/', search, name='search'),
 ]
--- Notice_board/notice/views.py
+++ Notice_board/notice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Notice
+from .forms import SearchForm
 
 
 def notice_list(request):
@@ -10,3 +11,14 @@ def notice_list(request):
 def notice_detail(request, not_id):
     notice = Notice.objects.get(id=not_id)
     return render(request, 'notice/not_detail.html', {'notice': notice})
+
+
+def search(request):
+    notice = Notice.objects.all()
+    form = SearchForm(request.GET)
+
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        notice = notice.filter(title__icontains=query)
+
+    return render(request, 'notice/search.html', {'form': form, 'notice': notice})
