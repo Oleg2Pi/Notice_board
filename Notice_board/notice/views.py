6fab7cb1b21be2af67210d366b7a286c5d5df0dc
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-04-02 00:44:16 +0300

    Create form of create notice and add in view, urls, created template:

--- Notice_board/notice/views.py
+++ Notice_board/notice/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Notice
-from .forms import SearchForm
+from .forms import SearchForm, NotForm
 
 
 def notice_list(request):
@@ -22,3 +23,17 @@ def search(request):
         notice = notice.filter(title__icontains=query)
 
     return render(request, 'notice/search.html', {'form': form, 'notice': notice})
+
+
+@login_required
+def created_not(request):
+    if request.method == 'POST':
+        form = NotForm(request.POST)
+        if form.is_valid():
+            notice = form.save(commit=False)
+            notice.person = request.user
+            notice.save()
+            return redirect('not_list')
+    else:
+        form = NotForm()
+    return render(request, 'notice/create_not.html', {'form': form})
